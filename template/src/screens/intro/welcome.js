c7e0eb0c386f46d75ede01f1e5f0f43e1b3a69e5
Thomas Schoffelen <thomas@schof.co>
2020-06-13 18:40:59 +0100

    Cleanup welcome screen.

--- template/src/screens/intro/welcome.js
+++ template/src/screens/intro/welcome.js
@@ -3,10 +3,30 @@ import { SafeAreaView, Text } from 'react-native';
 
 const WelcomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Hello, world!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
     </SafeAreaView>
   );
 };
 
+WelcomeScreen.options = {
+  topBar: {
+    title: {
+      text: 'Welcome'
+    }
+  }
+};
+
 export default WelcomeScreen;
+
+const styles = {
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  text: {
+    color: '#61dafb',
+    fontSize: 32
+  }
+};
