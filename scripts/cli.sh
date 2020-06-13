8d0cbc5963ec934f570fdddab6ef9f339d1e4b0f
Thomas Schoffelen <thomas@schof.co>
2020-06-13 19:36:37 +0100

    Fix CLI command.

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-RN_VERSION="0.63.0-rc.1"
-RN_TEMPLATE="@flexible-agency/react-native-starter"
-
-npx @react-native-community/cli init --template $RN_TEMPLATE --version $RN_VERSION $@
+npx @react-native-community/cli init $@ \
+  --template @flexible-agency/react-native-starter \
+  --version 0.63.0-rc.1
